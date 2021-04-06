992f2754fb3f4baf7a14eb9a6c2ee79b55f63c9b
FlyingSeaweed <39686099+KenM-FlyingSeaweed@users.noreply.github.com>
2021-04-06 23:59:28 +0900

    Initialize python file

--- timing_tool.py
+++ timing_tool.py
@@ -1,3 +1,16 @@
+import wx
 import serial
-import serial.tools import list_ports
+from serial.tools import list_ports
 import datetime
+
+def main():
+    ports = list_ports.comports()
+    print(ports)
+    for info in ports:
+        print(info.device)
+
+if __name__ == "__main__":
+    app = wx.App(True,None)
+    frame = wx.Frame(None, -1, 'Hello World', size=(500,500))
+    frame.Show()
+    app.MainLoop()
