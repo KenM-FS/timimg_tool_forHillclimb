a7b65888e3d6ea348d3b365ffc5d0aae03c6037b
FlyingSeaweed <39686099+KenM-FlyingSeaweed@users.noreply.github.com>
2021-06-05 21:03:06 +0900

    Minimum function

--- Arduino/timing_tool/timing_tool.ino
+++ Arduino/timing_tool/timing_tool.ino
@@ -1,5 +1,4 @@
 void setup() {
-  // put your setup code here, to run once:
   // disable Timer0 for stable serial com
   TIMSK0 = 0;
   pinMode(2, INPUT);
@@ -8,8 +7,8 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // it seems faster to use while with serial com
+  // minimun implementation to send signal
+  // it seems faster to use while in loop
   while(1){
     if(digitalRead(2) == HIGH){
       Serial.write(1);
--- timing_tool.py
+++ timing_tool.py
@@ -1,16 +1,85 @@
-import wx
+from datetime import date, datetime
+from datetime import timedelta
 import serial
-from serial.tools import list_ports
-import datetime
+import wx
+
+class MainFrame(wx.Frame):
+
+    def __init__(self, parent, ID, title):
+
+        wx.Frame.__init__(self, parent, title=title)
+        self.last_time = datetime.now() - timedelta(seconds=30)
+
+        self.__create_widget()
+        self.__do_layout()
+        self.SetSize((300,200))
+        self.Centre()
+
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.OnTimer)
+        self.timer.Start(1)
+
+    def __create_widget(self):
+        self.clock = wx.StaticText(self, label = "")
+
+        self.text = wx.StaticText(self, label = "計測時間")
+        self.check = wx.StaticText(self, label = "計測無し")
+
+        self.button = wx.Button(self, label = "手動計測")
+        self.button.Bind(wx.EVT_BUTTON, self.OnButton)
+
+        self.combobox = wx.ComboBox(self, choices = ["COM1_tmp", "COM2_tmp"], style=wx.CB_READONLY)
+
+    def __do_layout(self):
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.clock, flag=wx.ALIGN_LEFT)
+        sizer.Add(self.text, flag=wx.ALIGN_LEFT)
+        sizer.Add(self.check, flag=wx.ALIGN_LEFT)
+        sizer.Add(self.button, flag=wx.ALIGN_CENTER | wx.TOP, border=15)
+        sizer.Add(self.combobox, flag=wx.ALIGN_CENTER | wx.TOP, border=5)
+        self.SetSizer(sizer)
+
+    def OnButton(self, button_label):
+        current = datetime.now()
+
+        ts = (current.strftime('%H:%M:%S.%f')[:-3])
+        if current - self.last_time > timedelta(seconds=5):
+            self.check.SetLabel(ts)
+            self.last_time = current
+
+    def OnTimer(self, event):
+        current = datetime.now()
+
+        ts = (current.strftime('%H:%M:%S.%f')[:-3])
+        self.clock.SetLabel(ts)
+        line = arduino.read()
+        val = int.from_bytes(line, 'big')
+        if val == 1:
+            self.OnButton(self.button)
+        arduino.reset_input_buffer()
+
+class TimingApp(wx.App):
+
+    def OnInit(self):
+        # フレームオブジェクトの生成
+        frame = MainFrame(None, -1, "時間計測")
+        
+        # メインフレームに設定
+        self.SetTopWindow(frame)
+
+        # フレーム表示
+        frame.Show(True)
+        return True
 
 def main():
-    ports = list_ports.comports()
-    print(ports)
-    for info in ports:
-        print(info.device)
-
-if __name__ == "__main__":
-    app = wx.App(True,None)
-    frame = wx.Frame(None, -1, 'Hello World', size=(500,500))
-    frame.Show()
+    app = TimingApp()
     app.MainLoop()
+
+if __name__ == '__main__':
+    try:
+        arduino = serial.Serial('COM3', 9600, timeout=0)
+    except:
+        print("Failed to connect")
+        exit()
+
+    main()
